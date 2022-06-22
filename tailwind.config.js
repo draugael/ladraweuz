6bafa198518527114687b10054a084dbfc14f9a5
Draugael <mdraugael@gmail.com>
2022-06-22 18:41:11 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      'ladraweuz-blue': '#01496B',
+      'ladraweuz-ochre': '#D4A200',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
