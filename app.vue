5de052d9e8c62d6f5b4099fa077b2ded91859f8a
Draugael <mdraugael@gmail.com>
2022-06-26 17:55:05 +0200

    first text

--- app.vue
+++ app.vue
@@ -1,4 +1,36 @@
 <template>
+    <header class="flex flex-col items-center gap-3 mt-11 mb-16">
+        <img src="/asset/image/logo-ladraweuz.svg" alt="logo de Ladraweuz" loading="lazy" class="w-48">
+        <img src="/asset/image/title-ladraweuz.svg" alt="Ladraweuz: graphisme et illustration" loading="lazy"
+            class="w-64">
+    </header>
+    <main class="mb-auto">
+        <article class="flex flex-col items-center gap-5 font-fira text-center text-xl">
+            <p class="text-5xl">
+                Hello!
+            </p>
+            <p>
+                Moi, c’est Johanna ou <span class="text-ladraweuz-ochre">Ladraweuz</span>.<br>
+                Je suis <span class="text-ladraweuz-ochre">graphiste</span> et
+                <span class="text-ladraweuz-blue">illustratrice</span> freelance.<br>
+                J’habite à Clisson,merveilleuse ville médiévale du Hellfest !
+            </p>
+            <p>
+                Mon site est encore en construction...<br>
+                Désolée ;)
+            </p>
+            <p>
+                Appelez-moi si vous avez un projet de graphisme ou d’illustration, de logo, de dessin etc...<br>
+                Et retrouve moi sur les réseaux sociaux ;)
+            </p>
+        </article>
+        <section>
+
+        </section>
+    </main>
+    <footer class="font-fira text-center duration-150 ease-in-out opacity-50 hover:opacity-100">
+        <p>Made with ❤️ by Draugael</p>
+    </footer>
 </template>
 <style lang="scss">
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       'ladraweuz-blue': '#01496B',
       'ladraweuz-ochre': '#D4A200',
     },
+    fontFamily: {
+      fira: ['FiraSans-Regular', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
